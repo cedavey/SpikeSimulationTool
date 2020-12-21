8521dea52b7d4c0c0b463bed5a28520f810a934a
dron-tan <75568864+dron-tan@users.noreply.github.com>
2020-12-21 12:41:28 +1100

    Update printReport.m

--- printReport.m
+++ printReport.m
@@ -10,19 +10,27 @@ function report_op = printReport(report, dt)
 
 try
    if isfield(report, 'inf_time')
+      % Finding which fields have different values
       i = find(report.recruit > min(report.recruit));
       j = find(report.dismiss < max(report.dismiss));
+      
+      %Initialisation
       recruited_s = [];
       dismissed_s = [];
+      
+      % If find reports vector length 1, simply print that value
       if length(i) == 1
           recruited_s = num2str(i');
+          
+      % If length of find is larger than 1, iterate through its length to
+      % print correct statement
       elseif length(i) > 1
           recruited_s = [num2str(i(1))];
           for x = 2:length(i)
               recruited_s = [recruited_s ', ' num2str(i(x))];
           end
       end
-      
+      % Same as above but for dismissed
       if length(j) == 1
           dismissed_s = num2str(j');
       elseif length(j) > 1
