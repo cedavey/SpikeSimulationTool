5968d0cf2d5517710015ff2f20661d3b818f5ed5
dron-tan <75568864+dron-tan@users.noreply.github.com>
2020-12-24 12:21:21 +1100

    Update printReport.m
    - Removed uneccesary comments

--- printReport.m
+++ printReport.m
@@ -36,17 +36,3 @@ catch E
    report_op = sprintf('Couldn''t print the report. Unexpected event: %s\n', E.message);
 end
 end
-
-
-% try
-%    if isfield(report, 'inf_time')
-%       fprintf('\tInflammation: %.02f s| Number of inflamed axons: %d\n', report.inf_time * dt, numel(report.inflamed));
-%    else
-%       fprintf('\tNumber of inflamed axons: %d\n', 0);
-%    end
-%    fprintf('\tAmplitude change time: %.02f s\n', report.opts.Events.amplitude_dist_onset * dt);
-%    fprintf('\tRecruited axons: %s\n', num2str(find(report.recruit > min(report.recruit))'));
-%    fprintf('\tDismissed axons: %s\n', num2str(find(report.dismiss < max(report.dismiss))'));
-% catch E
-%    fprintf('\tCouldn''t print the report. Unexpected event: %s\n', E.message);
-% end 
