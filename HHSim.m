dd784f8409638833d75c344cf5a279cb0a2acb4e
chrisoyh <chrisong1234@gmail.com>
2021-01-12 23:49:35 +1100

    Overlapping spike option & bug fixes
    - Overlapping spikes option compatible with transition and non-transition
    
    - Fixed bug where spike simulator does not work when there are no transitions

--- HHSim.m
+++ HHSim.m
@@ -20,7 +20,7 @@ xInit    = [-65; 0.052; 0.059; 0.317];
 % Generate new template
 [~, new_template] = gen_template(t, x(:,1), duration);
 new_template = new_template + 65; % Adjust RMP to 0
-new_template = new_template / max(new_template); % Normalize
+new_template = new_template / max(abs(new_template)); % Normalize
 
 end
 
@@ -32,24 +32,5 @@ fr = 5000; %sampling rate
 % Interpolate data for constant sampling rate
 new_t = 0 : 1/fr*1000 : duration;   % Time vector
 new_template = interp1(t, data, new_t, 'spline');   % Value vector
-
-% % Differentiate the new_template to find start and end index
-% slope = diff(new_template)./diff(new_t);
-% 
-% % Do not extract template if no action potential is fired
-% if ~any(slope > 20) 
-%     new_t = 0; 
-%     new_template = 0; 
-%     return 
-% end
-% 
-% % If there is a large change in the first 10 ms, ignore these assuming the
-% % system is going back into equilibrium
-% start_ap = find(slope(10:end) >= 1, 1, 'first') + 10;   % Start AP index
-% end_ap = find(slope >=0.05 | slope <= -0.03, 1, 'last');    % End AP index
-% 
-% % Extracting only the relevant template
-% new_t = new_t(start_ap - 5:end_ap);
-% new_template = new_template(start_ap - 5:end_ap);
 
 end
