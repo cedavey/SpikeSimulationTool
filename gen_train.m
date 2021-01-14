8e1b7ede2a7c409907ca5572acfc97b36cffaca6
chrisoyh <chrisong1234@gmail.com>
2021-01-14 19:21:15 +1100

    Grouping transition spikes & Bug fixes
    gen_train.m
    - Transitioning spikes sorted into cell groups
    - Fixed duplicate times in transitions bug

--- gen_train.m
+++ gen_train.m
@@ -204,7 +204,7 @@ function [vv, report] = run_simulation(Naxons, templates, fs, duration ,opts ,am
          isi = ceil(isi);
       end
       
-      [sptimes, non_transition, transition] = seperate_transition_spikes(isi, size(templates.d,1));
+      [sptimes, non_transition, transition] = separate_transition_spikes(isi, size(templates.d,1));
       
       % Get spike times
       %sptimes = cumsum(isi);
@@ -296,28 +296,48 @@ end
 
 % Seperates the spikes which are close together and have transitions between them and
 % those far apart without transitions.
-function [sptimes, non_transition, transition] = seperate_transition_spikes(isi, duration_of_spike)
+function [sptimes, non_transition, transition] = separate_transition_spikes(isi, duration_of_spike)
 
 % Calculate times of all spikes
 sptimes = cumsum(isi);
 
-transition = [];
-non_transition = [];
-skip = 0;
+% Determine the times for spikes that do not transition
+non_transition    = sptimes(1);
+for i = 1 : length(isi)-1
+   if isi(i) <= duration_of_spike  || isi(i+1) <= duration_of_spike
+       % When the isi for the next 2 spikes is <= rest, adds the 
+       % current isi to the current cumulative sum
+       non_transition(end,1) = sptimes(i+1); 
+   elseif isi(i) > duration_of_spike
+       % When the isi for the spikes is > rest, it creates a new index to
+       % indicate the next normal (without transition) spike
+       non_transition(end+1,1) = sptimes(i+1);
+   else
+       error('Error: There is a missing value in sptimes_normal');
+   end
+   
+end
+% Start and end exceptions that the for loop cant handle properly
+% Since the first spike doesnt have a spike before it, it can be
+% non-transitioning when the isi after it is > rest 
+if ( isi(2) > duration_of_spike ) && ( non_transition(1) ~= sptimes(1) )
+    non_transition = [sptimes(1); non_transition]; 
+end
+% Clears the last sptimes_normal if the last isi is less than the rest time
+% since the for loop above can't check the preallocated last value (may
+% need to change this when considering total duration of simulation)
+if isi(end) <= duration_of_spike 
+    non_transition(end) = []; 
+end
 
-for i = 1:length(isi) - 1
-    if (isi(i + 1) - duration_of_spike) <= 0
-        transition = [transition; sptimes(i); sptimes(i+1)];
-        skip = 1;
-    elseif skip
-        skip = 0;
-        continue
-    else
-        non_transition = [non_transition; sptimes(i)];
-        if i == (length(isi) - 1)
-            non_transition = [non_transition; sptimes(i+1)];
-        end
-    end
+% Determines the times of spikes with transitions (mutually exclusive to
+% times without transitions)
+transition = sptimes(~ismember(sptimes, non_transition));
+
+% Find groups of transitioning spikes and split into cells
+group_start_n_end = [0; find(diff(transition) > duration_of_spike); length(transition)];
+for i = 1 : length(group_start_n_end) - 1
+    transition_cells{i} = transition(group_start_n_end(i)+1 : group_start_n_end(i+1));
 end
 
 end
