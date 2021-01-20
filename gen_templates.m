78acdd9944a167491ecf19b2b3f20d743718d0d8
dron-tan <75568864+dron-tan@users.noreply.github.com>
2021-01-20 11:54:44 +1100

    Update gen_templates.m
    Went through gen_templates and fixed issues that will help with implementation into SpikeSimulationTool.
    - Added indexing to help with this
    - Added transition.end_time, transition.end_index, transition.abs_refract_index
    
    Removed rest of unncessary code and created functions:
    - interpolate
    - adj_templates
    
    Made sampling_rate and initial_ap a variable in main code.
    
    + other minor adjustments

--- gen_templates.m
+++ gen_templates.m
@@ -7,208 +7,181 @@
 clear all
 close all
 
-% tic;
-    % Initialize channel constants
-    % All these also can be varied
-    const.vRest = -65;               % This can be varied later on
-    const.eNa   = 115 + const.vRest; % [mV]
-    const.gNa   = 120;
-    const.eK    = -12 + const.vRest; % [mV]
-    const.gK    = 36;
-    const.eLeak = 10.6 + const.vRest;% [mV]
-    const.gLeak = 0.3;
-    const.C     = 1.0;               % Capacitance of membrane
+% Initialize channel constants
+% All these also can be varied
+const.vRest = -65;               % This can be varied later on
+const.eNa   = 115 + const.vRest; % [mV]
+const.gNa   = 120;
+const.eK    = -12 + const.vRest; % [mV]
+const.gK    = 36;
+const.eLeak = 10.6 + const.vRest;% [mV]
+const.gLeak = 0.3;
+const.C     = 1.0;               % Capacitance of membrane
 
-    duration = 200; % [msec]
-    tInit    = [0 duration];
-    xInit    = [-65; 0.052; 0.059; 0.317];
+duration = 200; % [msec]
+tInit    = [0 duration];
+xInit    = [-65; 0.052; 0.059; 0.317];
+sampling_rate = 5000;
+initial_ap = 30;
 
-    % Iapp function
-    Iapp = @Iapp_func; % applied current injection (can be written as function of t)
+% Calculate the refractory period
+[templates.abs_refract_time, templates.abs_refract_index] = refract_period(tInit, xInit, const, duration, initial_ap, sampling_rate);
 
-    % Run ODE
-    [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+% Calculate when second input will generate same output as previous output
+% and generate transition templates
+[templates.end_time, templates.end_index, templates.transition] = gen_trans(tInit, xInit, const, duration, initial_ap, sampling_rate, templates);
 
-    % Generate new template
-    [templates.t, templates.d] = gen_template(t, x(:,1), duration);
-    
-    templates.refract_time = refract_period(tInit, xInit, const, duration);
-    
-    [templates.transition, templates.t, templates.d] = gen_trans(tInit, xInit, const, templates, duration);
+% Input function
+Iapp = @(t) 10*exp(-((t - initial_ap)*2).^2);
 
-    % Plot
-    plot_simulation(t, x, duration, Iapp, templates);
-    
-    % Save the template struct as .mat file
-    save_templates(templates);
-    
-% simulationTime = toc;
-% fprintf('Time elapsed = %f\n', simulationTime);
+% Runs ODE
+[t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
 
-%% Iapp function
-function Iapp_out = Iapp_func(t)
+% Interpolate data
+[interp_t, interp_d] = interpolate(t, x(:,1), duration, sampling_rate);
 
-% Bell curve function
-Iapp_out = 10 * exp(-((t-30)*2).^2);
-           %10 * exp(-((t-66)*2).^2);
-           %20 * exp(-((t-70)/2).^2); 
+% Generate single spike template
+[templates.t, templates.d] = gen_template(interp_t, interp_d, 1, templates);
 
+% Adjust the transition template lengths
+templates = adj_templates(templates, initial_ap);
 
-% % Constant function
-% Iapp_out = 0;
-% for i = 15:50
-%     Iapp_out = Iapp_out + 10 * exp(-((t - i)*2).^2);
-% end
+% Plot the simulation
+plot_simulation(t, x, duration, Iapp, templates);
 
-% Sine function
-%Iapp_out = 10*sin((t-10)/5);
+% Save the templates struct as a .mat file
+save_templates(templates);
 
-% Removes negative numbers
-%Iapp_out(Iapp_out < 0) = 0; 
+%% Interpolate data function
+
+% This function will interpolate the data to the correct sampling rate
+function [int_t, int_d] = interpolate(t, d, duration, sampling_rate)
+
+int_t = 0 : 1/sampling_rate*1000 : duration;
+int_d = interp1(t, d, int_t, 'spline');
 
 end
 
-%% Create new template function
-function [new_t, new_template] = gen_template(t, data, duration)
-fr = 5000; %sampling rate
+%% Calculate the absolute refractory period
 
-% Interpolate data for constant sampling rate
-new_t = 0 : 1/fr*1000 : duration;   % Time vector
-new_template = interp1(t, data, new_t, 'spline');   % Value vector
-
-% Spike template
-% Differentiate the new_template to find start and end index
-slope = diff(new_template)./diff(new_t);
-
-% Do not extract template if no action potential is fired
-if ~any(slope > 20) 
-    new_t = 0; 
-    new_template = 0; 
-    return 
-end
-
-% If there is a large change in the first 10 ms, ignore these assuming the
-% system is going back into equilibrium
-start_ap = find(slope(10:end) >= 1, 1, 'first') + 10;   % Start AP index
-
-% Function will determine between a single spike or two spikes to determine
-% where to fix end_ap as
-pks = findpeaks(data);
-pks = pks(pks >=0);
-if length(pks) >= 2 && diff(abs(pks([1,2]))) < 5
-    end_ap = find(slope >=0.05 | slope <= -0.05, 1, 'last');    % End AP index when 2 spikes
-else
-    end_ap = length(new_t); % When single spike
-end
-
-% Extracting only the relevant template
-new_t = new_t(start_ap - 5:end_ap);
-new_template = new_template(start_ap - 5:end_ap);
-
-end
-
-%% Calculate refractory period function
-
-function refract_time = refract_period(tInit, xInit, const, duration)
+function [refract_time, refract_index] = refract_period(tInit, xInit, const, duration, initial_ap, sampling_rate)
 
 refract = 0;
-initial_ap = 30;
-i = initial_ap + 1; % This corresponds to +1 of the initial input spike at t = 15
+i = initial_ap + 1; % 1+ to the initial_ap that was defined
+
+% Will loop until refract variable becomes true
 while refract == 0
-    Iapp = @(t) 10 * exp(-((t-initial_ap)*2).^2) + 10 * exp(-((t-i)*2).^2);
+    Iapp = @(t) 10 * exp(-((t - initial_ap) * 2).^2) + 10 * exp(-((t - i) * 2).^2);
     [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+    [int_t, int_d] = interpolate(t, x(:,1), duration, sampling_rate);
     
-    % Use findpeaks to find the first 2 peaks which will be the 2 aps
-    pks = findpeaks(x(:,1));
-    pks = pks(pks>=0);
-    if length(pks) >= 2 && diff(abs(pks([1,2]))) < 5
-        refract = 1;
-        refract_time = i - initial_ap;
+    % Use find peaks to find the first 2 peaks which will be the 2 APs
+    pks = findpeaks(int_d);
+    pks = pks(pks >= 0);
+    if length(pks) >= 2 && diff(abs(pks([1,2]))) < 5    % If more than 2 peaks above 0 and the diff between the two is less than 5
+        refract = 1;    % Change refract to be true to break while loop
+        refract_time = i - initial_ap;  % This refractory time is the time between the TWO INPUT SPIKES
+        refract_index = find(int_t >= i, 1, 'first') - find(int_t >= initial_ap, 1, 'first');   % This refractory index is the values within the time matrix between the TWO INPUT SPIKES
     end
     if i > duration
         error('Unable to find refractory time due to lack of second spike');
         refract_time = NaN;
+        refract_index = NaN;
     end
-    i = i + 1;
+    i = i + 1;   
 end
 end
 
-%% Generating templates for in between spikes for efficiency
-% This function will generate the entire template with transition periods.
-% This includes the two spikes that occur
+%% Generating the transition templates and calculating the end times/index
 
-% HOWEVER one issue that is not currently addressed (on the very very rare
-% occasion) is if whilst generating the action potential, if somehow a value
-% is == 0, then this will cause an irregularity in the template
-function [trans_temp, t, d] = gen_trans(tInit, xInit, const, templates, duration)
+function [end_time, end_index, trans_temp] = gen_trans(tInit, xInit, const, duration, initial_ap, sampling_rate, templates)
 
-% Initialising values
-% temp1 = [];   % Used for if want to store data in matrix
-trans_temp = {};    % Used for if want to store data in cell arrays
-initial_ap = 30;    % This is the first input spike
-% inx = 0;
+trans_temp = {};
+count = 0;
 
-% Iterate through this for the number of ms of templates you want
-% It is currently at 10 templates from right after the abs refractory
-% period
-for i = templates.refract_time:templates.refract_time + (size(templates.d, 2) - templates.refract_time)
-    Iapp = @(t) 10 * exp(-((t-initial_ap) * 2).^2) + 10 * exp(-((t-(initial_ap + i))*2).^2);
+% Loops through the entirety of the duration if required in steps of 0.2ms
+for i = templates.abs_refract_time + initial_ap:1/sampling_rate * 1000:duration
+    count = count + 1;
+    
+    Iapp = @(t) 10 * exp(-((t - initial_ap) * 2).^2) + 10 * exp(-((t - i)*2).^2);
     [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+    [int_t, int_d] = interpolate(t, x(:,1), duration, sampling_rate);
+    [~, temp_d] = gen_template(int_t, int_d, 2);
     
-    [temp2_t, temp2] = gen_template(t, x(:,1),(duration));
+    % Places data into cell arrays
+    temp_d = {temp_d - temp_d(1,1)};
+    trans_temp = [trans_temp temp_d];
     
-    % This uses cell arrays instead
-    temp2 = {temp2' - temp2(1,1)};
-    trans_temp = [trans_temp temp2];
-    
-    % Checks if the last 3 templates have the same max value. If they do
-    % then break out of this and record value to adjust the main template
-    if i > (templates.refract_time + 3) && (max(trans_temp{:, i - templates.refract_time}) == max(trans_temp{:, (i - templates.refract_time - 1)})) ...
-            && (max(trans_temp{:, i - templates.refract_time}) == max(trans_temp{:, (i - templates.refract_time - 2)})) ...
-            && (max(trans_temp{:, (i - templates.refract_time)}) == max(trans_temp{:, (i - templates.refract_time - 3)}))
-        trans_temp = {trans_temp{:, 1:i - templates.refract_time - 3}};
+    % Checks if the last 3 templates (in intervals of 5) have the same max 
+    % value. If they do then it breaks out of loop and records value to adjust templates
+    if (i > templates.abs_refract_time + initial_ap + 3) ...
+            && (max(trans_temp{:, count}) == max(trans_temp{:, count - 5})) ...
+            && (max(trans_temp{:, count}) == max(trans_temp{:, count - 10})) ...
+            && (max(trans_temp{:, count}) == max(trans_temp{:, count - 15}))
+        trans_temp = {trans_temp{:, 1:count - 15}};
+        end_time = i - 3;   % End time is absolute time (includes initial_ap time)
+        end_index = find(int_t == i) - 15;  % Relative to INTERPOLATED index (includes initial_ap)
         break
     end
     
-    % Code below will store templates in a matrix
-%     temp2 = temp2';
-    
-    % This little bit will concatenate different size matricies and fill in
-    % the gaps with 0's
-%     [i1, j1] = ndgrid(1:size(temp1, 1), 1:size(temp1, 2));
-%     [i2, j2] = ndgrid(1:size(temp2, 1), (1:size(temp2, 2)) + size(temp1, 2));
-%     temp1 = accumarray([i1(:), j1(:); i2(:), j2(:)], [temp1(:); temp2(:)]);
+end
 end
 
-% Adjust the template length
-t = templates.t(1:find(temp2_t >= initial_ap + i - 3, 1, 'first'));
-d = templates.d(1:length(t));
+%% Template generator function
 
-% Find all the 0s (which is 99% of the time going to be just at the end of
-% temp1 and change then to the initial value and then normalise the whole
-% array
-% temp1(find(temp1 == 0)) = temp1(1,1);
-% trans_temp = temp1 - temp1(1,1);
+function [new_t, new_d] = gen_template(t, d, num_inputs, templates)
+
+% Differentiate new_d to help find start and end index
+slope = diff(d)./diff(t);
+
+% If no AP is fired, do not generate template
+if ~any(slope > 20)
+    new_t = 0;
+    new_d = 0;
+    return
 end
 
-%% Save function
-% Saves the struct template into a .mat file for SST to open
-function save_templates(templates)
+start_ap = 145; % Start AP index. This index's as 7 in the interpolated time data
 
-% This will then inverse the arrays to work with main code
-templates.d = (templates.d + abs(templates.d(1)))';
-templates.t = templates.t';
-
-[file, path] = uiputfile(['templates' filesep 'template_test2.mat'], 'Save file name'); % Will want to change the name
-if file
-    file_name = [path filesep file];
-    save(file_name, 'templates');
+% Will adjust end_ap depending on the number of inputs
+if num_inputs == 1
+    end_ap = templates.end_index;
+    %end_ap = find(slope >= 0.05 | slope <= -0.05, 1, 'last');
 else
-    fprintf('User didnt''t chose a file location. Template was not saved\n');
+    end_ap = length(t); % If there are 2 inputs, then it will adjust template later
+    
+end
+
+% Adjusts the template
+new_t = t(start_ap : end_ap)';
+new_d = (d(start_ap : end_ap))';
+
+end
+
+%% Adjust templates.transition templates
+
+% Currently only works if there are MAXIMUM of 2 inputs
+function templates = adj_templates(templates, initial_ap)
+
+start_ap = 145;     % This is just a variable from the gen_templates
+initial_ap_index = find(templates.t == initial_ap);
+
+% This is the number of positions between input spike and end index which
+% will correspond to second input spike and end index for the second input
+% spike
+% Assumes that the following spike dynamics are same as the first
+index_bw_ap = templates.end_index - start_ap;
+
+% Runs through the transition templates and shortens them to the correct
+% length
+for i = 1:size(templates.transition,2)
+    templates.transition{i} = templates.transition{i}(1:initial_ap_index ...
+        + templates.abs_refract_index - 1 + i + index_bw_ap); 
 end
 end
 
-%% PLOT function
+%% Plot function
+
 function plot_simulation(t, x, duration, Iapp, template)
 % Plot action potential
 
@@ -250,4 +223,20 @@ title('Channel activations');
 legend('m', 'h', 'n');
 hold off
 
+end
+
+%% Save function
+% Saves the struct template into a .mat file for SST to open
+function save_templates(templates)
+
+% This will then inverse the arrays to work with main code
+templates.d = templates.d + abs(templates.d(1));
+
+[file, path] = uiputfile(['templates' filesep 'template_test2.mat'], 'Save file name'); % Will want to change the name
+if file
+    file_name = [path filesep file];
+    save(file_name, 'templates');
+else
+    fprintf('User didnt''t chose a file location. Template was not saved\n');
+end
 end
