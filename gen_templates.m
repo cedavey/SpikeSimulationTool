2d1123a87d03dd5bac01724f3f96cbfaedef1f2b
dron-tan <75568864+dron-tan@users.noreply.github.com>
2021-01-26 10:29:25 +1100

    Added initial_ap_index variable
    Added the initial_ap_index variable to the templates struct

--- gen_templates.m
+++ gen_templates.m
@@ -22,17 +22,17 @@ duration = 200; % [msec]
 tInit    = [0 duration];
 xInit    = [-65; 0.052; 0.059; 0.317];
 sampling_rate = 5000;
-initial_ap = 30;
+templates.initial_ap = 30;
 
 % Calculate the refractory period
-[templates.abs_refract_time, templates.abs_refract_index] = refract_period(tInit, xInit, const, duration, initial_ap, sampling_rate);
+[templates.abs_refract_time, templates.abs_refract_index] = refract_period(tInit, xInit, const, duration, templates, sampling_rate);
 
 % Calculate when second input will generate same output as previous output
 % and generate transition templates
-[templates.end_time, templates.end_index, templates.transition] = gen_trans(tInit, xInit, const, duration, initial_ap, sampling_rate, templates);
+[templates.end_time, templates.end_index, templates.transition] = gen_trans(tInit, xInit, const, duration, sampling_rate, templates);
 
 % Input function
-Iapp = @(t) 10*exp(-((t - initial_ap)*2).^2);
+Iapp = @(t) 10*exp(-((t - templates.initial_ap)*2).^2);
 
 % Runs ODE
 [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
@@ -44,7 +44,7 @@ Iapp = @(t) 10*exp(-((t - initial_ap)*2).^2);
 [templates.t, templates.d] = gen_template(interp_t, interp_d, 1, templates);
 
 % Adjust the transition template lengths
-templates = adj_templates(templates, initial_ap);
+templates = adj_templates(templates);
 
 % Plot the simulation
 plot_simulation(t, x, duration, Iapp, templates);
@@ -64,14 +64,14 @@ end
 
 %% Calculate the absolute refractory period
 
-function [refract_time, refract_index] = refract_period(tInit, xInit, const, duration, initial_ap, sampling_rate)
+function [refract_time, refract_index] = refract_period(tInit, xInit, const, duration, templates, sampling_rate)
 
 refract = 0;
-i = initial_ap + 1; % 1+ to the initial_ap that was defined
+i = templates.initial_ap + 1; % 1+ to the initial_ap that was defined
 
 % Will loop until refract variable becomes true
 while refract == 0
-    Iapp = @(t) 10 * exp(-((t - initial_ap) * 2).^2) + 10 * exp(-((t - i) * 2).^2);
+    Iapp = @(t) 10 * exp(-((t - templates.initial_ap) * 2).^2) + 10 * exp(-((t - i) * 2).^2);
     [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
     [int_t, int_d] = interpolate(t, x(:,1), duration, sampling_rate);
     
@@ -80,8 +80,8 @@ while refract == 0
     pks = pks(pks >= 0);
     if length(pks) >= 2 && diff(abs(pks([1,2]))) < 5    % If more than 2 peaks above 0 and the diff between the two is less than 5
         refract = 1;    % Change refract to be true to break while loop
-        refract_time = i - initial_ap;  % This refractory time is the time between the TWO INPUT SPIKES. AT this time is when the first following spike can occur
-        refract_index = find(int_t >= i, 1, 'first') - find(int_t >= initial_ap, 1, 'first');   % This refractory index is the values within the time matrix between the TWO INPUT SPIKES
+        refract_time = i - templates.initial_ap;  % This refractory time is the time between the TWO INPUT SPIKES. AT this time is when the first following spike can occur
+        refract_index = find(int_t >= i, 1, 'first') - find(int_t >= templates.initial_ap, 1, 'first');   % This refractory index is the values within the time matrix between the TWO INPUT SPIKES
     end
     if i > duration
         error('Unable to find refractory time due to lack of second spike');
@@ -94,16 +94,16 @@ end
 
 %% Generating the transition templates and calculating the end times/index
 
-function [end_time, end_index, trans_temp] = gen_trans(tInit, xInit, const, duration, initial_ap, sampling_rate, templates)
+function [end_time, end_index, trans_temp] = gen_trans(tInit, xInit, const, duration, sampling_rate, templates)
 
 trans_temp = {};
 count = 0;
 
 % Loops through the entirety of the duration if required in steps of 0.2ms
-for i = templates.abs_refract_time + initial_ap:1/sampling_rate * 1000:duration
+for i = templates.abs_refract_time + templates.initial_ap:1/sampling_rate * 1000:duration
     count = count + 1;
     
-    Iapp = @(t) 10 * exp(-((t - initial_ap) * 2).^2) + 10 * exp(-((t - i)*2).^2);
+    Iapp = @(t) 10 * exp(-((t - templates.initial_ap) * 2).^2) + 10 * exp(-((t - i)*2).^2);
     [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
     [int_t, int_d] = interpolate(t, x(:,1), duration, sampling_rate);
     [~, temp_d] = gen_template(int_t, int_d, 2);
@@ -114,7 +114,7 @@ for i = templates.abs_refract_time + initial_ap:1/sampling_rate * 1000:duration
     
     % Checks if the last 3 templates (in intervals of 5) have the same max 
     % value. If they do then it breaks out of loop and records value to adjust templates
-    if (i > templates.abs_refract_time + initial_ap + 3) ...
+    if (i > templates.abs_refract_time + templates.initial_ap + 3) ...
             && (max(trans_temp{:, count}) == max(trans_temp{:, count - 5})) ...
             && (max(trans_temp{:, count}) == max(trans_temp{:, count - 10})) ...
             && (max(trans_temp{:, count}) == max(trans_temp{:, count - 15}))
@@ -156,15 +156,18 @@ end
 new_t = t(start_ap : end_ap)';
 new_d = (d(start_ap : end_ap))';
 
+% Find the initial_ap index
+
+
 end
 
 %% Adjust templates.transition templates
 
 % Currently only works if there are MAXIMUM of 2 inputs
-function templates = adj_templates(templates, initial_ap)
+function templates = adj_templates(templates)
 
 start_ap = 145;     % This is just a variable from the gen_templates
-initial_ap_index = find(templates.t == initial_ap);
+templates.initial_ap_index = find(templates.t == templates.initial_ap) - 1;  % - 1 because of weird matlab indexing
 
 % This is the number of positions between input spike and end index which
 % will correspond to second input spike and end index for the second input
@@ -179,8 +182,8 @@ max_val = max(cellfun(@(x) max(x), templates.transition));
 % Runs through the transition templates and shortens them to the correct
 % length
 for i = 1:size(templates.transition,2)
-    templates.transition{i} = templates.transition{i}(1:initial_ap_index ...
-        + templates.abs_refract_index - 1 + i + index_bw_ap);
+    templates.transition{i} = templates.transition{i}(1:templates.initial_ap_index ...
+        + templates.abs_refract_index + i + index_bw_ap);
     
     % Normalises the templates amplitude so that the max peak is = 1
     temp = templates.transition{i};
