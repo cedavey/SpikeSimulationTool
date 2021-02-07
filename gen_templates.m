5adcafdb317cc5a8229d728f2ecbfe4ca3f52a17
dron-tan <75568864+dron-tan@users.noreply.github.com>
2021-02-07 12:21:39 +1100

    Fixing integration
    Fixing the integration due to renaming of files

--- gen_templates.m
+++ gen_templates.m
@@ -1,85 +1,90 @@
-% Modified from teaching material from Workshop 3 Neural Information Processing
-% University of Mebourne
-% Darren Tan 910828
-
-% Function that generates and plots action potentials using the Hodgkin-Huxley model
-
-clear all
-%close all
-
-% Initialize channel constants
-% All these also can be varied
-const.vRest = -65;               % This can be varied later on
-const.eNa   = 115 + const.vRest; % [mV]
-const.gNa   = 120;
-const.eK    = -11 + const.vRest; % [mV]
-const.gK    = 36;
-const.eLeak = 10.6 + const.vRest;% [mV]
-const.gLeak = 0.3;
-const.C     = 1.0;               % Capacitance of membrane
+function [templates] = gen_templates(parameters)
 
 duration = 200; % [msec]
 tInit    = [0 duration];
 xInit    = [-65; 0.052; 0.059; 0.317];
-templates.sampling_rate = 12000;
+% sampling_rate = 5000;   % Add this into parameters
 templates.initial_ap = 30;
-templates.initial_ap_index = find(0:1/templates.sampling_rate*1000:duration >= templates.initial_ap, 1, 'first');
+templates.initial_ap_index = find(0:1/parameters.sampling_rate*1000:duration >= templates.initial_ap, 1, 'first');
 
-% Store parameters for future reference
+% Progress bar
+w = waitbar(0, 'Generating templates...');
 
-templates.parametersUsed = store(const, templates);
+% Store parameters into the templates struct for reference
+templates.parametersUsed = store(parameters);
 
 % Calculate the refractory period
-[templates.abs_refract_time, templates.abs_refract_index, templates.rel_refract_time, templates.rel_refract_index] = refract_period(tInit, xInit, const, duration, templates);
+[templates.abs_refract_time, templates.abs_refract_index, templates.rel_refract_time, templates.rel_refract_index] = refract_period(tInit, xInit, duration, templates, parameters, w);
+
+try w = waitbar(2/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Calculate when second input will generate same output as previous output
 % and generate transition templates
-templates.transition = gen_trans(tInit, xInit, const, duration, templates);
+templates.transition = gen_trans(tInit, xInit, duration, templates, parameters);
+
+try w = waitbar(3/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Input function
-Iapp = @(t) 10*exp(-((t - templates.initial_ap)*2).^2) + 10*exp(-((t - 50)*2).^2);
+Iapp = @(t) 10*exp(-((t - templates.initial_ap)*2).^2);
+
+try w = waitbar(4/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Runs ODE
-[t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+[t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, parameters);
+
+try w = waitbar(5/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Interpolate data
-[interp_t, interp_d] = interpolate(t, x(:,1), duration, templates.sampling_rate);
+[interp_t, interp_d] = interpolate(t, x(:,1), duration, parameters);
+
+try w = waitbar(6/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Generate single spike template
 [templates.t, templates.d] = gen_template(interp_t, interp_d, 1, templates);
+% Change to column vector and normalize to 0 and normalize max
+templates.d = (templates.d + abs(templates.d(1)));
+templates.d = templates.d / max(templates.d);
+
+try w = waitbar(7/8, w); catch, delete(w); error('Manually stopped'); end
 
 % Adjust the transition template lengths
 templates = adj_templates(templates);
 
-% Plot the simulation
-plot_simulation(t, x, duration, Iapp, templates);
+% Insert the sampling rate
+templates.sampling_rate = parameters.sampling_rate;
 
-% Save the templates struct as a .mat file
-%save_templates(templates);
+try w = waitbar(8/8, w); catch, delete(w); error('Manually stopped'); end
+
+% Close progress bar
+try delete(w); catch E, fprintf(2,'\t%s\n',E.message); end
+
+end
 
 %% Interpolate data function
 
 % This function will interpolate the data to the correct sampling rate
-function [int_t, int_d] = interpolate(t, d, duration, sampling_rate)
+function [int_t, int_d] = interpolate(t, d, duration, parameters)
 
-int_t = 0 : 1/sampling_rate*1000 : duration;
+int_t = 0 : 1/parameters.sampling_rate*1000 : duration;
 int_d = interp1(t, d, int_t, 'spline');
 
 end
 
 %% Calculate the absolute refractory period
 
-function [refract_time, refract_index, rel_refract_time, rel_refract_index] = refract_period(tInit, xInit, const, duration, templates)
+function [refract_time, refract_index, rel_refract_time, rel_refract_index] = refract_period(tInit, xInit, duration, templates, parameters, w)
 
-max_sampling_rate = 100000; % Aribitrary number right now
+max_sampling_rate = 100000;
+buffer = 0.0001;
+
 refract = 0;
-i = templates.initial_ap + 1; % 1+ to the initial_ap that was defined
+i = templates.initial_ap; % 1+ to the initial_ap that was defined
 
 % Will loop until refract variable becomes true
 while refract == 0
     i = i + 1/max_sampling_rate*1000; % This time should be 1/sampling_rate
     Iapp = @(t) 10 * exp(-((t - templates.initial_ap) * 2).^2) + 10 * exp(-((t - i) * 2).^2);
-    [~, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+    [~, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, parameters);
 %     [int_t, int_d] = interpolate(t, x(:,1), duration, templates.sampling_rate);
     
     % Use find peaks to find the first 2 peaks which will be the 2 APs
@@ -88,8 +93,8 @@ while refract == 0
     if length(pks) >= 2 && diff(abs(pks([1,2]))) < 5    % If more than 2 peaks above 0 and the diff between the two is less than 5
         refract = 1;    % Change refract to be true to break while loop
         max_sr_refract_time = i - templates.initial_ap;
-        refract_index = find(0:1/templates.sampling_rate*1000:duration >= i - templates.initial_ap, 1, 'first');  % This refractory time is the time between the TWO INPUT SPIKES. AT this time is when the first following spike can occur
-        refract_time = 1/templates.sampling_rate*1000 * (refract_index - 1);   % This refractory index is the values within the time matrix between the TWO INPUT SPIKES
+        refract_index = find(0:1/parameters.sampling_rate*1000:duration >= i - templates.initial_ap, 1, 'first');  % This refractory time is the time between the TWO INPUT SPIKES. AT this time is when the first following spike can occur
+        refract_time = 1/parameters.sampling_rate*1000 * (refract_index - 1);   % This refractory index is the values within the time matrix between the TWO INPUT SPIKES
     end
     if i > 60
         error('Unable to find refractory time due to lack of second spike');
@@ -98,50 +103,46 @@ while refract == 0
     end
 end
 
+try w = waitbar(1/8, w); catch, delete(w); error('Manually stopped'); end
+
 % Finding the relative refractory period at max sampling rate to keep
 % consistency
 count = 0;
 rel_temp = [];
-buffer = 0.0001;
 
 for i = max_sr_refract_time + templates.initial_ap:1/max_sampling_rate * 1000:duration
     count = count + 1;
     Iapp = @(t) 10 * exp(-((t - templates.initial_ap) * 2).^2) + 10 * exp(-((t - i)*2).^2);
-    [~, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
+    [~, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, parameters);
     d = x(:,1);
     rel_d = {d - d(1)};
     rel_temp = [rel_temp rel_d];
     
-    if (i > max_sr_refract_time + templates.initial_ap + 6)...
+    if (i > max_sr_refract_time + templates.initial_ap + 5)...
             && ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - max_sampling_rate/1000}) - buffer))...
             && ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 2}) - buffer))...
             && ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 3}) - buffer))...
             && ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 4}) - buffer))...
             && ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 5}) - buffer))
-            %&& ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 6}) - buffer))
-            %&& ((max(rel_temp{:, count}) <= max(rel_temp{:, count - max_sampling_rate/1000}) + buffer) && (max(rel_temp{:, count}) >= max(rel_temp{:, count - (max_sampling_rate/1000) * 7}) - buffer))
-        rel_refract_index = find(0:1/templates.sampling_rate * 1000:duration >= (i - 5 - templates.initial_ap), 1, 'first');   % Takes first value that is larger than the one found with max_sampling_rate. This value is FROM initial spike at index = 151
-        rel_refract_time = 1/templates.sampling_rate * 1000 * (rel_refract_index - 1);    % This value is FROM the initial spike at t = 30
+        rel_refract_index = find(0:1/parameters.sampling_rate * 1000:duration >= (i - 5 - templates.initial_ap), 1, 'first');   % Takes first value that is larger than the one found with max_sampling_rate. This value is FROM initial spike at index = 151
+        rel_refract_time = 1/parameters.sampling_rate * 1000 * (rel_refract_index - 1);    % This value is FROM the initial spike at t = 30
         break
     end
 end
-
-
-
 end
 
 %% Generating the transition templates and calculating the end times/index
 
-function trans_temp = gen_trans(tInit, xInit, const, duration, templates)
+function trans_temp = gen_trans(tInit, xInit, duration, templates, parameters)
 
 trans_temp = {};
 
 % Loops through the entirety of the duration if required in steps of 0.2ms
-for i = templates.abs_refract_time + templates.initial_ap:1/templates.sampling_rate * 1000:templates.rel_refract_time+templates.initial_ap
+for i = templates.abs_refract_time + templates.initial_ap:1/parameters.sampling_rate * 1000:templates.rel_refract_time+templates.initial_ap
     
     Iapp = @(t) 10 * exp(-((t - templates.initial_ap) * 2).^2) + 10 * exp(-((t - i)*2).^2);
-    [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, const);
-    [int_t, int_d] = interpolate(t, x(:,1), duration, templates.sampling_rate);
+    [t, x] = ode45('gen_templates_HHode', tInit, xInit, [], Iapp, parameters);
+    [int_t, int_d] = interpolate(t, x(:,1), duration, parameters);
     [~, temp_d] = gen_template(int_t, int_d, 2, templates);
     
     % Places data into cell arrays
@@ -215,76 +216,13 @@ end
 
 %% Store the parameters
 
-function parametersUsed = store(const, templates)
+function parametersUsed = store(parameters)
 
-parametersUsed = {};
+parametersUsed{1} = {['Sampling rate = ' num2str(parameters.sampling_rate)]};
+parametersUsed{2} = {['eNa = ' num2str(parameters.eNa - parameters.vRest)] ['gNa = ' num2str(parameters.gNa)]};
+parametersUsed{3} = {['eK = ' num2str(parameters.eK - parameters.vRest)] ['gK = ' num2str(parameters.gK)]};
+parametersUsed{4} = {['eLeak = ' num2str(parameters.eLeak - parameters.vRest)] ['gLeak = ' num2str(parameters.gLeak)]};
+parametersUsed{5} = {['C = ' num2str(parameters.C)]};
+parametersUsed{6} = {['vRest = ' num2str(parameters.vRest)]};
 
-parametersUsed{1} = {['Sampling rate = ' num2str(templates.sampling_rate)]};
-parametersUsed{2} = {['eNa = ' num2str(const.eNa - const.vRest)] ['gNa = ' num2str(const.gNa)]};
-parametersUsed{3} = {['eK = ' num2str(const.eK - const.vRest)] ['gK = ' num2str(const.gK)]};
-parametersUsed{4} = {['eLeak = ' num2str(const.eLeak - const.vRest)] ['gLeak = ' num2str(const.gLeak)]};
-parametersUsed{5} = {['C = ' num2str(const.C)]};
-parametersUsed{6} = {['vRest = ' num2str(const.vRest)]};
-
-end
-
-%% Plot function
-
-function plot_simulation(t, x, duration, Iapp, template)
-% Plot action potential
-
-subplot(2,1,1);
-hold on
-yyaxis left
-plot(t, x(:,1));
-axis([0 duration -80 60]);
-xlabel('time (ms)');
-ylabel('voltage (mV)');
-
-% Plot current applied
-Iapp_ = Iapp(t);
-if numel(Iapp_) == 1; Iapp_ = Iapp_*ones(size(t)); end % Changes a constant Iapp_ to an array for plotting
-
-% Plot new template
-plot(template.t, template.d, '-r');
-
-yyaxis right
-p = plot(t, Iapp_, '-');
-p.Color(4) = 0.2; % Change transparency
-axis([0, duration, -(4*max(abs(Iapp_))), (3*max(abs(Iapp_)))]);
-xlabel('time (ms)');
-ylabel('Current applied');
-
-legend('AP voltage', 'Extracted template','Applied current');
-hold off
-
-% Plot the m, h and n variables
-subplot(2,1,2);
-hold on
-plot(t, x(:,2));
-plot(t, x(:,3));
-plot(t, x(:,4));
-ylim([-0.1 1.1]);
-xlabel('Time (ms)');
-ylabel('Activation');
-title('Channel activations');
-legend('m', 'h', 'n');
-hold off
-
-end
-
-%% Save function
-% Saves the struct template into a .mat file for SST to open
-function save_templates(templates)
-
-% This will then inverse the arrays to work with main code
-templates.d = templates.d + abs(templates.d(1));
-
-[file, path] = uiputfile(['templates' filesep 'templates_test3.mat'], 'Save file name'); % Will want to change the name
-if file
-    file_name = [path filesep file];
-    save(file_name, 'templates');
-else
-    fprintf('User didnt''t chose a file location. Template was not saved\n');
-end
 end
